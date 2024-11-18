7a8aa868341dc869de04abc71a2e49d6c3882dea
Javier Palacios <javijpa@hotmail.com>
2024-11-18 21:26:51 +0100

    .

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\34687\AppData\Local\Temp\tmprlb32vpj.js
+// include: C:\Users\34687\AppData\Local\Temp\tmp85whlfdp.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -219,7 +219,7 @@ Module['FS_createPath']("/", "shaders", true, true);
 
   })();
 
-// end include: C:\Users\34687\AppData\Local\Temp\tmprlb32vpj.js
+// end include: C:\Users\34687\AppData\Local\Temp\tmp85whlfdp.js
 
 
 // Sometimes an existing Module object exists with properties
